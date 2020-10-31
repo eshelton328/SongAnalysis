53300f0df90c48688edfd525f300fa4f1bf04d96
Erik Shelton <erik.shelton328@gmail.com>
2020-10-31 11:05:46 -0600

    Created a SearchBar component. Installed a few dependencies. Organized file sturcture

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import logo from '../logo.svg';
+import SearchBar from './SearchBar.js'
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
+
+export default App;
