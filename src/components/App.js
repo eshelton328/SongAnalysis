f6329200562780d5e9bc13661e68f3d37c3d2318
Erik Shelton <erik.shelton328@gmail.com>
2020-10-31 14:29:43 -0600

    Connected search bar and submit component with functionality
    Connected application to spotify api. After typing in text to the search bar and clicking submit, spotify api will send back the 20 most relevent items back to the client

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,33 @@
-import logo from '../logo.svg';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
 import SearchBar from './SearchBar.js'
 import '../styles/App.css';
 
 function App() {
   return (
     <div>
-      <SearchBar />
+      <Container>
+        <Grid
+            container
+            direction="row"
+            justify="center"
+        >
+          <Grid
+            container
+            direction="row"
+            justify="center"
+          >
+            <h1>CrowdPleaser</h1>
+          </Grid>
+          <Grid
+            item
+            xs={5}
+          >
+            <SearchBar />
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
